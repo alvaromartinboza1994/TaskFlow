d31580660781043e1e542ea4edb0fc1f9d23b454
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2025-11-06 16:42:24 +0100

    [P1]: repository interfaces

---
+++ src/main/java/com/taskflow/repository/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.taskflow.repository;
+
+import com.taskflow.model.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
